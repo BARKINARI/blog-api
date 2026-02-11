97a245b03d3d10fa67638aaa90b35eb7ffc038c0
Barkın Arı <brkn3342@gmail.com>
2026-02-11 13:27:33 +0300

    feat: add Models

---
+++ Blog.Api/Models/Comment.cs
@@ -0,0 +1,12 @@
+namespace Blog.Api.Models;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public string? Text { get; set; }
+    public string? Author { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public int PostId { get; set; }
+    public Post Post { get; set; } = null!;
+}
---
+++ Blog.Api/Models/Post.cs
@@ -0,0 +1,13 @@
+namespace Blog.Api.Models;
+
+public class Post
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Content { get; set; }
+    public string? Author { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
+
+    public List<Comment> Comments { get; set; } = new();
+}
