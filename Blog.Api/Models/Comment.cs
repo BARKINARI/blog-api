83cec23921628a0f5eb0438b6837f1c5676c0546
Barkın Arı <brkn3342@gmail.com>
2026-02-12 19:19:53 +0300

    fix: add string.Empty

--- Blog.Api/Models/Comment.cs
+++ Blog.Api/Models/Comment.cs
@@ -3,8 +3,8 @@ namespace Blog.Api.Models;
 public class Comment
 {
     public int Id { get; set; }
-    public string? Text { get; set; }
-    public string? Author { get; set; }
+    public string Text { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
     public int PostId { get; set; }
--- Blog.Api/Models/Post.cs
+++ Blog.Api/Models/Post.cs
@@ -3,9 +3,9 @@ namespace Blog.Api.Models;
 public class Post
 {
     public int Id { get; set; }
-    public string? Title { get; set; }
-    public string? Content { get; set; }
-    public string? Author { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? UpdatedAt { get; set; }
 
